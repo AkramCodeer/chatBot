af76d8219e432b7de2761284a7fae8373c4a8782
Akram-khan1 <akram.k@palletnow.co>
2024-08-19 22:16:37 +0530

    commit

---
+++ vue-material-dashboard-laravel/src/examples/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="py-3 footer">
+   
+   
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "app-footer",
+};
+</script>
---
+++ vue-material-dashboard-laravel/src/examples/Sidenav/SidenavList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="w-auto h-auto collapse navbar-collapse max-height-vh-100 h-100" id="sidenav-collapse-main">
+    <ul class="navbar-nav">
+
+      <!-- Dashboard -->
+      <li class="nav-item">
+        <sidenav-collapse url="#" :aria-controls="''" v-bind:collapse="false" collapseRef="dashboard" navText="Dashboard">
+          <template v-slot:icon>
+            <i class="material-icons-round opacity-10 fs-5">dashboard</i>
+          </template>
+        </sidenav-collapse>
+      </li>
+
+      <!-- PhoneBot -->
+      <li class="nav-item">
+        <sidenav-collapse url="#" :aria-controls="''" v-bind:collapse="false" collapseRef="phonebot" navText="PhoneBot">
+          <template v-slot:icon>
+            <i class="material-icons-round opacity-10 fs-5">smartphone</i>
+          </template>
+        </sidenav-collapse>
+      </li>
+
+      <!-- Sign In -->
+      <li class="nav-item">
+        <sidenav-collapse url="#" :aria-controls="''" v-bind:collapse="false" collapseRef="sign-in" navText="Sign In">
+          <template v-slot:icon>
+            <i class="material-icons-round opacity-10 fs-5">login</i>
+          </template>
+        </sidenav-collapse>
+      </li>
+
+      <!-- Sign Up -->
+      <li class="nav-item">
+        <sidenav-collapse url="#" :aria-controls="''" v-bind:collapse="false" collapseRef="sign-up" navText="Sign Up">
+          <template v-slot:icon>
+            <i class="material-icons-round opacity-10 fs-5">assignment</i>
+          </template>
+        </sidenav-collapse>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+import SidenavCollapse from "./SidenavCollapse.vue";
+
+export default {
+  name: "SidenavList",
+  components: {
+    SidenavCollapse
+  }
+};
+</script>
